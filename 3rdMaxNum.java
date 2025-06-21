c303a3f031cf10daabd91207de9af1ca583e805c
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-21 20:49:57 +0700

    Update 3rdMaxNum.java

--- 3rdMaxNum.java
+++ 3rdMaxNum.java
@@ -2,18 +2,18 @@ import java.util.TreeSet;
 
 class Solution {
     public int thirdMax(int[] nums) {
-       TreeSet<Integer> set = new TreeSet<>();
+       TreeSet<Integer> set = new TreeSet<>();// store the number in ascending order (smallest to biggest)
 
        for(int num : nums){
-        set.add(num);
-        if(set.size() > 3){
+        set.add(num);//store all the number in set
+        if(set.size() > 3){//if the set size is more then 3 it's remove the first number is the smallest
             set.remove(set.first());
         }
        }
-        if(set.size() < 3){
+        if(set.size() < 3){//if the set size less than 3 then return the last one which is the largest one 
             return set.last();
 
        }
-       return set.first();
+       return set.first();//this return the 3rd largest number which is what we want
        }
        }
